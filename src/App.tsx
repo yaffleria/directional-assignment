82a8f6331588b773d408c060c7cfaba6ed6ec8ee
Jungmin Ji <jungmin.ji@dreams.io>
2025-11-30 12:53:07 +0900

    chore: initialize project with react, typescript and tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <h1 className="text-3xl font-bold text-blue-500">
+        Directional Assignment
+      </h1>
+    </div>
+  );
+}
+
+export default App;
