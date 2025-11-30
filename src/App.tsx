d69e4dc7fca2543050c90a32945131adc923e4bc
Jungmin Ji <jungmin.ji@dreams.io>
2025-11-30 15:45:41 +0900

    feat: Implement protected routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import PostsListPage from "./pages/PostsListPage";
 import PostDetailPage from "./pages/PostDetailPage";
 import PostFormPage from "./pages/PostFormPage";
 import DashboardPage from "./pages/DashboardPage";
+import { ProtectedRoute } from "./components/ProtectedRoute";
 
 function App() {
   return (
@@ -12,11 +13,15 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
-        <Route path="/posts" element={<PostsListPage />} />
-        <Route path="/posts/new" element={<PostFormPage />} />
-        <Route path="/posts/:id" element={<PostDetailPage />} />
-        <Route path="/posts/:id/edit" element={<PostFormPage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
+
+        <Route element={<ProtectedRoute />}>
+          <Route path="/posts" element={<PostsListPage />} />
+          <Route path="/posts/new" element={<PostFormPage />} />
+          <Route path="/posts/:id" element={<PostDetailPage />} />
+          <Route path="/posts/:id/edit" element={<PostFormPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+        </Route>
+
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
