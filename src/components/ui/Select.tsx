295fb72adfee6e8e52ad89857477df20b16e67be
Jungmin Ji <jungmin.ji@dreams.io>
2025-11-30 16:05:59 +0900

    fix: resolve Select component type error

--- src/components/ui/Select.tsx
+++ src/components/ui/Select.tsx
@@ -1,15 +1,14 @@
 import * as React from "react";
 import { cn } from "../../lib/utils";
 
-export interface SelectProps
-  extends React.SelectHTMLAttributes<HTMLSelectElement> {}
+export type SelectProps = React.SelectHTMLAttributes<HTMLSelectElement>;
 
 const Select = React.forwardRef<HTMLSelectElement, SelectProps>(
   ({ className, ...props }, ref) => {
     return (
       <select
         className={cn(
-          "rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background",
+          "border border-input bg-background px-3 py-2 text-sm ring-offset-background",
           "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
           "disabled:cursor-not-allowed disabled:opacity-50",
           className
